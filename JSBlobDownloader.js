e76afb8825b0f0a56b5d2aba44a4ec675c01a2f7
Jonathan <joniweb01@gmail.com>
2018-03-05 00:45:10 -0300

    Added downloadChunked, fixes #1

--- JSBlobDownloader.js
+++ JSBlobDownloader.js
@@ -1,19 +1,19 @@
 /**
 
 	JsBlobDownloader
-	
+
 	Description:
-	 
-	This script will be facilitate to the website monitor the download 
-	with more precise information, example, you can know if a user has 
-	downloaded completely file and the velocity of this download, 
+
+	This script will be facilitate to the website monitor the download
+	with more precise information, example, you can know if a user has
+	downloaded completely file and the velocity of this download,
 	of course you have to know to do it to measure velocity.
-	
-    Copyright (C) 2015   JonathanScripter
+
+    Copyright (C) 2018   JonathanScripter
 
  	Licensed under the MIT license:
 	http://www.opensource.org/licenses/mit-license.php
-	
+
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
@@ -29,7 +29,7 @@
 var NewBlob = function(data, datatype) {
 	var out;
 	try {
-		out = new Blob([ data ], {
+		out = new Blob(data, {
 			type : datatype
 		});
 	} catch (e) {
@@ -37,10 +37,12 @@ var NewBlob = function(data, datatype) {
 				|| window.MozBlobBuilder || window.MSBlobBuilder;
 		if (e.name == 'TypeError' && window.BlobBuilder) {
 			var bb = new BlobBuilder();
-			bb.append(data);
+			for (i in data) {
+				bb.append(i);
+			}
 			out = bb.getBlob(datatype);
 		} else if (e.name == "InvalidStateError") {
-			out = new Blob([ data ], {
+			out = new Blob(data, {
 				type : datatype
 			});
 		} else {
@@ -51,20 +53,90 @@ var NewBlob = function(data, datatype) {
 }
 // NewBlob (compatibility): http://stackoverflow.com/a/15335607
 
-
 function download(file, onFinish, updateProgressBar, mimeType) {
-	mimeType = (typeof mimeType !== 'undefined' ? mimeType : 'application/octet-stream');
-	var req = new XMLHttpRequest();
-	req.onprogress = updateProgressBar;
-	req.open('GET', file, true);
-	req.responseType = 'arraybuffer';
-	req.onload = function(e) {
-		if (this.status == 200) {
-			var blob = new NewBlob(this.response, mimeType);
-			onFinish(window.URL.createObjectURL(blob));
-		}
-	};
-	req.send();
+	downloadChunked(file, 1, onFinish, updateProgressBar, mimeType);
 }
 
+function downloadChunked(file, chunks, onFinish, updateProgressBar, mimeType) {
+	mimeType = (typeof mimeType !== 'undefined' ? mimeType : 'application/octet-stream');
+	new Promise(resolve => {
+	  var xhr = new XMLHttpRequest();
+	  xhr.open('GET', file, true);
+	  xhr.onreadystatechange = () => {
+	      resolve(+xhr.getResponseHeader("Content-Length"));
+	      xhr.abort();
+	  };
+	  xhr.send();
+	}).then(file_size => {
+		var blob = null;
+		var size = 0;
+		var downloaded = 0;
+		var currentChunk = 1;
 
+		var size_per_chunk = parseInt(file_size / chunks, 10);
+
+		function retryDownload() {
+			setTimeout(tryDownload, 2000);
+		}
+
+		var updater = function(evt) {
+			var prog = function() {};
+			prog.lengthComputable = evt.lengthComputable;
+			prog.loaded = evt.loaded + size;
+			prog.total = file_size;
+			updateProgressBar(prog);
+		}
+
+		function tryDownload() {
+			try {
+				var req = new XMLHttpRequest();
+				req.timeout = 10000;
+				req.onprogress = updater;
+				req.open('GET', file, true);
+				var range = "";
+
+				if (currentChunk == chunks) {
+					range = size + "-";
+				} else {
+					range = size + "-" + (size_per_chunk * currentChunk);
+				}
+				req.setRequestHeader("Range", "bytes="+range);
+				req.overrideMimeType(mimeType);
+
+				req.responseType = 'arraybuffer';
+				req.onload = function(e) {
+					if (this.readyState == 4 && req.response) {
+						try {
+							if (blob) {
+								blobb = new NewBlob([this.response], mimeType);
+								size += blobb.size;
+								blob = new NewBlob([blob, blobb], mimeType);
+							} else {
+								blob = new NewBlob([this.response], mimeType);
+								size += blob.size;
+							}
+
+							if (currentChunk == chunks) {
+								onFinish(window.URL.createObjectURL(blob));
+							} else {
+								currentChunk++;
+								tryDownload();
+							}
+						} catch (e) {
+							retryDownload();
+						}
+					}
+				};
+				req.onreadystatechange = function() {
+					if (req.readyState == 4 && !req.response) {
+						retryDownload();
+					}
+				};
+				req.send();
+			} catch(e) {
+				retryDownload();
+			}
+		}
+		tryDownload();
+	});
+}
--- README.md
+++ README.md
@@ -6,3 +6,17 @@ This script will save file in blob cache, and this file can be download, or if i
 
 
 [Examples](http://jsu.zz.mu/jsblobdownloader)
+
+
+Chunked
+================
+
+Since 05/03/2018 (DD/MM/YYYY), JSBlobDownloader supports chunked download (requested by @usherfiles, issue #1), resume is not possible because JSBlobDownloader uses XHR (XMLHttpRequest) which does not allow to retrieve data if download is interrupted (I've considered using Streams API, but it requires to be explicit enabled in Firefox, so, it is not the better choice). However, if the download of chunk 7 is interrupted by the server, the script will retry to download this particular chunk, so the entire data does not need to be downloaded again.
+
+How to use:
+
+```
+downloadChunked(file, chunks, onFinish, updateProgressBar);
+```
+
+The example 1 and example 4 in the [website](http://jsu.zz.mu/jsblobdownloader) uses this feature, hover on download button and click in `<>` in right side of screen to see the source.
